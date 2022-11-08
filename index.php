92f6cb97bc1089236d28ce72be5b169dc42e19e1
ммđ αмıи ᴾᴴᴾ <51942680+Mr-Basirnia@users.noreply.github.com>
2022-11-08 21:56:48 +0330

    :sparkles: feat(router): stupid router.
    * Create stupid router for framework.

--- index.php
+++ index.php
@@ -1,17 +1,11 @@
 <?php
 
-use App\Utilities\Url;
+use App\Core\StupidRouter;
 
 
 // framework initiation file
 require 'bootstrap/bootstrap.php';
 
 
-$route = Url::current_route();
-
-if ($route == '/red')
-    include BASEPATH . '/views/colors/red.php';
-if ($route == '/green')
-    include BASEPATH . '/views/colors/green.php';
-if ($route == '/blue')
-    include BASEPATH . '/views/colors/blue.php';
+$router = new StupidRouter();
+$router->run();
